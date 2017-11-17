f9b040f304ec78533cc18e61047f4134139203d4
Patrick Runyan <jpwrunyan@hotmail.com>
2017-11-16 20:03:44 -0500

    added SystemUser model, controller, repo

---
+++ octo-user-service/src/main/java/com/revature/octo/user/model/SystemUser.java
@@ -0,0 +1,159 @@
+package com.revature.octo.user.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name="SYSTEM_USERS")
+@Component
+public class SystemUser implements UserDetails {
+	/**
+	 * Version 1.0
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	//@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="USER_ID_SEQ")
+	//@SequenceGenerator(sequenceName="user_seq", allocationSize=1, name="USER_ID_SEQ")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private boolean isEnabled;
+	private boolean isAccountNonExpired;
+	private boolean isAccountNonLocked;
+	private boolean isCredentialsNonExpired;
+	
+	/*
+	@OneToOne
+	@JoinColumn(name="ROLE_ID")
+	public UserRole role;
+	*/
+	
+	public SystemUser() {
+		
+	}
+	
+	public SystemUser(int id, String username, String password, boolean isEnabled, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.isEnabled = isEnabled;
+		this.isAccountNonExpired = isAccountNonExpired;
+		this.isAccountNonLocked = isAccountNonLocked;
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+	}
+
+	public SystemUser(String username, String password, boolean isEnabled, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.isEnabled = isEnabled;
+		this.isAccountNonExpired = isAccountNonExpired;
+		this.isAccountNonLocked = isAccountNonLocked;
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	public boolean isAccountNonExpired() {
+		return isAccountNonExpired;
+	}
+
+	public void setAccountNonExpired(boolean isAccountNonExpired) {
+		this.isAccountNonExpired = isAccountNonExpired;
+	}
+
+	public boolean isAccountNonLocked() {
+		return isAccountNonLocked;
+	}
+
+	public void setAccountNonLocked(boolean isAccountNonLocked) {
+		this.isAccountNonLocked = isAccountNonLocked;
+	}
+
+	public boolean isCredentialsNonExpired() {
+		return isCredentialsNonExpired;
+	}
+
+	public void setCredentialsNonExpired(boolean isCredentialsNonExpired) {
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority("role.getRole()"));
+		return authorities;
+	}
+	
+	@Override
+	public String toString() {
+		return "SystemUser [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
+				+ ", password=" + password + ", isEnabled=" + isEnabled + ", isAccountNonExpired=" + isAccountNonExpired
+				+ ", isAccountNonLocked=" + isAccountNonLocked + ", isCredentialsNonExpired=" + isCredentialsNonExpired
+				+ "]";
+	}
+}
