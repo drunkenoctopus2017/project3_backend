8553a25222d5bb4ead218eaed1875fbe2764ef60
Benjamin Rogers <brogers0101@gmail.com>
2017-11-22 11:02:31 -0500

    fixed merge issues, develop into newBranch = success

---
+++ octo.sql
@@ -0,0 +1,8 @@
+insert into system_user values (2, 'Elvis', 1, 1, 1, 1, 'Yang', 'password', 'eusername', 200);
+/
+insert into system_user values (5, 'Jibril', 1, 1, 1, 1, 'Burleigh', 'password', 'jusername', 200);
+/
+insert into system_user values (4, 'Ben', 1, 1, 1, 1, 'Rogers', 'password', 'busername', 200);
+/
+commit;
+/
