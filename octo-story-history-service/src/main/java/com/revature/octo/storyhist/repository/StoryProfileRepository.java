7883b37639fce474619c52d6e6318b433d9cf3d8
Patrick Runyan <jpwrunyan@hotmail.com>
2017-11-26 12:32:08 -0500

    initial story history service implementation

---
+++ octo-story-history-service/src/main/java/com/revature/octo/storyhist/repository/StoryProfileRepository.java
@@ -0,0 +1,11 @@
+package com.revature.octo.storyhist.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.revature.octo.storyhist.model.StoryProfile;
+
+public interface StoryProfileRepository extends CrudRepository<StoryProfile, Integer> {
+	List<StoryProfile> findByIdIn(List<Integer> storyIds);
+}
