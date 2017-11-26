9cbf68c5ab1df5ede283a047ba66c65fccfff8bb
Patrick Runyan <jpwrunyan@hotmail.com>
2017-11-26 15:24:00 -0500

    fix to artifact id and new message service

---
+++ octo-story-update-manager/src/main/java/com/revature/storyupdater/OctoStoryUpdateManagerApplication.java
@@ -0,0 +1,12 @@
+package com.revature.storyupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoStoryUpdateManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoStoryUpdateManagerApplication.class, args);
+	}
+}
