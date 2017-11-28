ff64cf4d9318aae0c6a9f01c96fbb1c8ede6f79b
waveeyang <waveeyang@gmail.com>
2017-11-28 16:57:39 -0500

    createUpdate Task backend method

--- octo-task-service/src/main/java/com/revature/octo/task/controller/TaskController.java
+++ octo-task-service/src/main/java/com/revature/octo/task/controller/TaskController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.octo.task.model.Task;
@@ -35,6 +38,11 @@ public class TaskController {
 	@GetMapping(path="/getTaskByStoryId/{storyId}")
 	public List<Task> getTaskByStoryId(@PathVariable int storyId){
 		return taskRepo.getTaskByStoryId(storyId);
+	}
+	
+	@PostMapping(path="/createUpdateTask",consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Task createUpdateTask(@RequestBody Task t) {
+		return taskRepo.save(t);
 	}
 	
 	@GetMapping("/createTestTasks")
