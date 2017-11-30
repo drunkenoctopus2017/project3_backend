c3cdf892cb452e801d59b259d0486976471b5cec
JibrilBurleigh <jibbgh@gmail.com>
2017-11-30 13:51:16 -0500

    added the oauth2 security settings

--- octo-story-history-service/src/main/java/com/revature/octo/storyhist/OctoChartServiceApplication.java
+++ octo-story-history-service/src/main/java/com/revature/octo/storyhist/OctoChartServiceApplication.java
@@ -3,9 +3,11 @@ package com.revature.octo.storyhist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableResourceServer
 public class OctoChartServiceApplication {
 
 	public static void main(String[] args) {
--- octo-user-service/src/main/java/com/revature/octo/user/OctoUserServiceApplication.java
+++ octo-user-service/src/main/java/com/revature/octo/user/OctoUserServiceApplication.java
@@ -2,11 +2,12 @@ package com.revature.octo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableResourceServer
 public class OctoUserServiceApplication {
 
 	public static void main(String[] args) {
