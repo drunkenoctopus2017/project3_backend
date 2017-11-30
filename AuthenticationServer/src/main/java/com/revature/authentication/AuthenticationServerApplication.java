ed22fd17822595b2fd9a7dde1e58e2f312cbe152
waveeyang <waveeyang@gmail.com>
2017-11-30 14:56:10 -0500

    working OAuth for users microservice

--- AuthenticationServer/src/main/java/com/revature/authentication/AuthenticationServerApplication.java
+++ AuthenticationServer/src/main/java/com/revature/authentication/AuthenticationServerApplication.java
@@ -2,11 +2,12 @@ package com.revature.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(allowCredentials="true", allowedHeaders="*")
 @SpringBootApplication
-
+@EnableEurekaClient
 public class AuthenticationServerApplication {
 
 	public static void main(String[] args) {
