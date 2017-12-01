04fd0c10b878858b654cc0e1c4324aa72068c884
Patrick Runyan <jpwrunyan@hotmail.com>
2017-11-30 19:28:42 -0500

    deleted obsolete story-update-manager

--- octo-story-update-manager/src/main/java/com/revature/storyupdater/message/StoryUpdateMessageSourceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.revature.storyupdater.message;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(StoryUpdateMessageSource.class)
-public class StoryUpdateMessageSourceImpl {
-
-}
